ba07411873e435b39c0869af402b5829ad7fd1de
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2023-03-29 10:07:44 -0300

    feat: roteiro de revisão de Recursão

---
+++ src/main/java/recursao/MetodosRecursivos.java
@@ -0,0 +1,70 @@
+package recursao;
+
+public class MetodosRecursivos {
+
+	public int calcularSomaArray(int[] array){
+		int result = 0;
+		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR A SOMA
+		// DOS EMENTOS DE UM ARRAY
+		return result;
+	}
+	public long calcularFatorial(int n) {
+		if(n == 1 || n == 0) {
+			return n;
+		}
+		return calcularFatorial(n-1) * n;
+	}
+
+	public int calcularFibonacci(int n) {
+		if(n == 1 || n == 2) {
+			return 1;
+		}
+		return calcularFibonacci(n - 1) + calcularFibonacci(n - 2);
+
+	}
+
+	public int countNotNull(Object[] array, int index) {
+		if(index < 0) {
+			return 0;
+		}
+		Object element = array[index];
+		if(element != null) {
+			return countNotNull(array, index - 1) + 1;
+		} else {
+			return countNotNull(array, index - 1);
+		}
+	}
+
+	public long potenciaDe2(int expoente) {
+		if(expoente < 0) {
+			throw new RuntimeException("Expoente não pode ser um númeiro inteiro negativo");
+		}
+		if(expoente == 0) {
+			return 1;
+		}
+		if(expoente == 1) {
+			return 2;
+		}
+		return potenciaDe2(expoente - 1) * 2;
+	}
+
+	public double progressaoAritmetica(double termoInicial, double razao, int n) {
+		if(n == 1) {
+			return  termoInicial;
+		}
+		double proximo = termoInicial + razao;
+		return progressaoAritmetica(proximo, razao, n - 1);
+
+
+	}
+
+	public double progressaoGeometrica(double termoInicial, double razao, int n) {
+		if(n == 1) {
+			return  termoInicial;
+		}
+		double proximo = termoInicial * razao;
+		return progressaoGeometrica(proximo, razao, n - 1);
+	}
+	
+	
+}
---
+++ src/main/java/recursao/TestarMetodosRecursivos.java
@@ -0,0 +1,24 @@
+package recursao;
+
+
+public class TestarMetodosRecursivos {
+
+	public static void main(String[] args) {
+		MetodosRecursivos metodos = new MetodosRecursivos();
+		System.out.println("Questão 1:");
+		System.out.println(metodos.calcularFatorial(5));
+		System.out.println("Questão 2:");
+		System.out.println(metodos.calcularFibonacci(8));
+		System.out.println("Questão 3:");
+		String[] array = {"1", "2", null, null, "5"};
+		System.out.println(metodos.countNotNull(array, array.length - 1));
+		System.out.println("Questão 4:");
+		System.out.println(metodos.potenciaDe2(4));
+		System.out.println("Questão 5:");
+		System.out.println("PA: 3, 5, 7, 9, 11");
+		System.out.println(metodos.progressaoAritmetica(3, 2, 5));
+		System.out.println("Questão 6:");
+		System.out.println("PG: 4, 12, 36, 108, 324");
+		System.out.println(metodos.progressaoGeometrica(4, 3, 5));
+	}
+}
